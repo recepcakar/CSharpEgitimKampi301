6599ebf9f7ad3cc06114d74122b71828ef850d96
recep <rrecepcakar@gmail.com>
2025-02-08 20:40:16 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{     
+    //interface internal olabilir 
+    public interface IGenericDal<T> where T : class // burada T class tipi olmalı kısıtlamasını getiriyoruz
+    {
+        void insert(T entity); // varsayılan olarak public o yüzden yazmamalıyız tek public olabiliyor zaten
+         void delete(int id);
+        void update(T entity);
+        List<T> getAll();
+        T GetById(int id);
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public string CatogoryName { get; set; }
+        public int CategoryId { get; set; }
+        public bool CatogoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+    /*
+    field=>  sınıfın içinde direkt
+    variable=> bir method içinde ise
+    property=> get set alırsa
+     */
+}
+
